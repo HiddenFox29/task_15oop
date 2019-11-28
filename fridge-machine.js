2c5c014a65be8e4b573543058dc2e5d7095c6b00
Aleksandr Alekseev <hiddenfox29@yandex.ru>
2019-11-28 19:58:03 +0600

    fix indentention

--- fridge-machine.js
+++ fridge-machine.js
@@ -5,44 +5,44 @@ function FrigeMachine(power) { // класс наследник от Machine
         this.__foodList = []; // приватная переменная стиль prototype ('__')
 };
 
-    FrigeMachine.prototype = Object.create(Machine.prototype); // наследование родителя через протатип
+FrigeMachine.prototype = Object.create(Machine.prototype); // наследование родителя через протатип
 
-    // создание методов класса с помощью протоатипов
-    FrigeMachine.prototype.addFood = function (food) { // добавлет еду
-        if(this.__enabled){
-            if(food){
-                if(this.__foodList.length < this._power/100) {
-                    this.__foodList.push(food);
-                    return this.getFoodList();
-                } else {
-                    throw new Error('ошибка! вы привысили макс количество подуктов в холодильнике');
-                }
-            } else {
-                throw new Error('Добавьте продукт');
-            }
-        } else {
-            throw new Error('ошибка! нельзя ложиль еду в отключенный холодильник');
-        }
-    };
-
-    FrigeMachine.prototype.getFoodList = function () { // список еды
-        if(this._enabled){
-            return this.__foodList.slice();
-        } else {
-            throw new Error('ошибка! нельзя получать список еды из отключенного холодильника')
-        }
-    };
-
-    FrigeMachine.prototype.takeFood = function (food) { // забераем еду
-        if(this._enabled){
-            let indexFood = this.__foodList.indexOf(food);
-            if(this.__foodList[indexFood] === food) {
-                this.__foodList.splice(indexFood, 1);
+// создание методов класса с помощью протоатипов
+FrigeMachine.prototype.addFood = function (food) { // добавлет еду
+    if(this.__enabled){
+        if(food){
+            if(this.__foodList.length < this._power/100) {
+                this.__foodList.push(food);
                 return this.getFoodList();
             } else {
-                throw new Error('ошибка! такого продукта в холодильника нет');
+                throw new Error('ошибка! вы привысили макс количество подуктов в холодильнике');
             }
         } else {
-            throw new Error('ошибка! нельзя забирать еду из отключенного холодильника');
+            throw new Error('Добавьте продукт');
         }
-    };
+    } else {
+        throw new Error('ошибка! нельзя ложиль еду в отключенный холодильник');
+    }
+};
+
+FrigeMachine.prototype.getFoodList = function () { // список еды
+    if(this._enabled){
+        return this.__foodList.slice();
+    } else {
+        throw new Error('ошибка! нельзя получать список еды из отключенного холодильника')
+    }
+};
+
+FrigeMachine.prototype.takeFood = function (food) { // забераем еду
+    if(this._enabled){
+        let indexFood = this.__foodList.indexOf(food);
+        if(this.__foodList[indexFood] === food) {
+            this.__foodList.splice(indexFood, 1);
+            return this.getFoodList();
+        } else {
+            throw new Error('ошибка! такого продукта в холодильника нет');
+        }
+    } else {
+        throw new Error('ошибка! нельзя забирать еду из отключенного холодильника');
+    }
+};
--- machine.js
+++ machine.js
@@ -3,12 +3,12 @@ function Machine(power) { // родительский класс
     this._power = power; // приватный метод класса
     this._enabled = false;
 };
-    Machine.prototype.enable = function () { // публичный метод
-        console.log('Machine enabled');
-        this._enabled = true;
-    };
+Machine.prototype.enable = function () { // публичный метод
+    console.log('Machine enabled');
+    this._enabled = true;
+};
 
-    Machine.prototype.disable = function () {
-        console.log('Machine disabled');
-        this._enabled = false;
-    };
+Machine.prototype.disable = function () {
+    console.log('Machine disabled');
+    this._enabled = false;
+};
